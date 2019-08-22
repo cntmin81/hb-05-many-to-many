092c8afe1d95bd95ca28c974bfd9565a164a6a99
YOUNGMIN SHIN <youngminshin@YOUNGMINui-MacBookPro.local>
2019-08-23 05:54:45 +0900

    Many to many add annotations into course and student class, add main class

--- src/com/luv2code/hibernate/demo/entity/Course.java
+++ src/com/luv2code/hibernate/demo/entity/Course.java
@@ -37,9 +37,20 @@ public class Course {
 	@JoinColumn(name = "course_id")
 	private List<Review> reviews;
 
-	@ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
-			CascadeType.REFRESH })
-	@JoinTable(name = "student_course", joinColumns = @JoinColumn(name = "course_id"), inverseJoinColumns = @JoinColumn(name = "student_id"))
+	@ManyToMany(
+			fetch = FetchType.LAZY, 
+			cascade = { 
+					CascadeType.DETACH, 
+					CascadeType.MERGE, 
+					CascadeType.PERSIST,
+					CascadeType.REFRESH
+					}
+			)
+	@JoinTable(
+			name = "course_student", 
+			joinColumns = @JoinColumn(name = "course_id"), 
+			inverseJoinColumns = @JoinColumn(name = "student_id")
+			)
 	private List<Student> students;
 
 	public Course() {
